bf8a437ff138efe55bcf8503e08abc948cf98174
YasirBajwa <yasirbajwa369@gmail.com>
2020-06-16 14:14:32 +0500

    update app

--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -20,11 +20,11 @@ export const Expenses = () => {
     <div className="inc-exp-container">
         <div>
           <h4>Income</h4>
-  <p className="money plus">{income}</p>
+  <p className="money plus"> ${income}</p>
         </div>
         <div>
           <h4>Expense</h4>
-  <p className="money minus">{expense}</p>
+  <p className="money minus">${expense}</p>
         </div>
       </div>
   )
